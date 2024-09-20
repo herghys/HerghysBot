0a5fc817946ca6e8ef916a75c553a98988817d93
Herghys <helmi.santosa1@gmail.com>
2024-09-20 23:55:53 +0700

    update

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,6 @@
-const cacheName = "Graphie-Duckie Bubble-1.05";
-const contentToCache = [
-    "Build/DuckieBubble.loader.js",
-    "Build/DuckieBubble.framework.js",
-    "Build/DuckieBubble.data",
-    "Build/DuckieBubble.wasm",
-    "TemplateData/style.css"
-
-];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
