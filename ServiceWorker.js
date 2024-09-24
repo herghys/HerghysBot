c1d78ab43921bf2acdca3c20c5d9a980e73f5f20
Herghys <helmi.santosa1@gmail.com>
2024-09-24 12:06:24 +0700

    a

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,19 +2,5 @@
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-    e.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    console.log('[Service Worker] Deleting cache:', cacheName);
-                    return caches.delete(cacheName);
-                })
-            );
-        }).then(() => {
-            // Optionally, skip waiting to immediately activate the new service worker
-            return self.skipWaiting();
-        })
-    );
-
 });
 
